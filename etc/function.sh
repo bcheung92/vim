27869ca822f357839d38453c7adb9164f35e7c14
skywind3000 <skywind3000@163.com>
2018-02-02 18:52:38 +0800

    update functions

--- etc/function.sh
+++ etc/function.sh
@@ -224,15 +224,15 @@ function q-extract
 {
 	if [ -f $1 ] ; then
 		case $1 in
-		*.tar.bz2)   tar xvjf $1    ;;
-		*.tar.gz)    tar xvzf $1    ;;
-		*.tar.xz)    tar xvJf $1    ;;
+		*.tar.bz2)   tar -xvjf $1    ;;
+		*.tar.gz)    tar -xvzf $1    ;;
+		*.tar.xz)    tar -xvJf $1    ;;
 		*.bz2)       bunzip2 $1     ;;
 		*.rar)       rar x $1       ;;
 		*.gz)        gunzip $1      ;;
-		*.tar)       tar xvf $1     ;;
-		*.tbz2)      tar xvjf $1    ;;
-		*.tgz)       tar xvzf $1    ;;
+		*.tar)       tar -xvf $1     ;;
+		*.tbz2)      tar -xvjf $1    ;;
+		*.tgz)       tar -xvzf $1    ;;
 		*.zip)       unzip $1       ;;
 		*.Z)         uncompress $1  ;;
 		*.7z)        7z x $1        ;;
@@ -249,11 +249,11 @@ function q-compress
 	if [ -n "$1" ] ; then
 		FILE=$1
 		case $FILE in
-		*.tar) shift && tar cf $FILE $* ;;
-		*.tar.bz2) shift && tar cjf $FILE $* ;;
-		*.tar.xz) shift && tar cJf $FILE $* ;;
-		*.tar.gz) shift && tar czf $FILE $* ;;
-		*.tgz) shift && tar czf $FILE $* ;;
+		*.tar) shift && tar -cf $FILE $* ;;
+		*.tar.bz2) shift && tar -cjf $FILE $* ;;
+		*.tar.xz) shift && tar -cJf $FILE $* ;;
+		*.tar.gz) shift && tar -czf $FILE $* ;;
+		*.tgz) shift && tar -czf $FILE $* ;;
 		*.zip) shift && zip $FILE $* ;;
 		*.rar) shift && rar $FILE $* ;;
 		esac
